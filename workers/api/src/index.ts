a77a26aca2736ceb51a503c1288069e2e1904d4c
dolphub <randy.mikkelsaar@gmail.com>
2019-12-15 23:16:33 -0800

    fixup! Adding routing, paths, kv access

--- workers/api/src/index.ts
+++ workers/api/src/index.ts
@@ -1,9 +1,34 @@
-import { router } from './routes';
+import tini from '@helloandre/tini';
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
+tini(router => {
+  router.get('/hello', req => {
+    return { hello: 'world' };
+  });
+  router.get('/:key', req => {
+    // outputs "myKey, 1"
+    return `${req.params.key}`;
+  });
 });
+// const random = () => Math.floor(Math.random() * 100);
 
-async function handleRequest(request: Request): Promise<Response> {
-  return router.route(request);
-}
+// const defaultHeaders = { 'Access-Control-Allow-Origin': '*', Vary: 'Origin' };
+
+// const jsonHeaders = {
+//   'content-type': 'application/json',
+// };
+
+// const response = {
+//   json: (data: {} | null) =>
+//     new Response(typeof data === 'string' ? data : JSON.stringify(data), {
+//       status: 200,
+//       headers: { ...defaultHeaders, ...jsonHeaders },
+//     }),
+// };
+
+// addEventListener('fetch', event => {
+//   event.respondWith(handleRequest(event.request));
+// });
+
+// async function handleRequest(request: Request): Promise<Response> {
+//   return response.json('Hello');
+// }
--- workers/api/src/routes.ts
+++ workers/api/src/routes.ts
@@ -1,45 +1,30 @@
-import Router from './worker-router';
-import { KVNamespace } from '@cloudflare/workers-types';
+// import Router from './worker-router';
+// import { KVNamespace } from '@cloudflare/workers-types';
 
-import WorkerRouter from './worker-router';
+// import WorkerRouter from './worker-router';
 
-declare const ns1: KVNamespace;
+// declare const ns1: KVNamespace;
 
-const random = () => Math.floor(Math.random() * 100);
+// const router = new WorkerRouter();
+// router.get('/', () => response.json('hi'));
+// router.get('/random', generateRandomNumber);
+// router.get('/data', getData);
+// router.get('/data/:val', getData);
+// router.put('/data', putData);
 
-const defaultHeaders = { 'Access-Control-Allow-Origin': '*', Vary: 'Origin' };
+// async function getData(req: Request) {
+//   const data = await ns1.get('test');
+//   return response.json(data);
+// }
 
-const jsonHeaders = {
-  'content-type': 'application/json',
-};
+// async function putData(req: Request) {
+//   const body = await req.json();
+//   const result = await ns1.put('test', JSON.stringify(body));
+//   return response.json({ body: result });
+// }
 
-const response = {
-  json: (data: {} | null) =>
-    new Response(typeof data === 'string' ? data : JSON.stringify(data), {
-      status: 200,
-      headers: { ...defaultHeaders, ...jsonHeaders },
-    }),
-};
+// async function generateRandomNumber(req: Request) {
+//   return response.json({ data: random() });
+// }
 
-const router = new WorkerRouter();
-router.get('/random', generateRandomNumber);
-router.get('/data', getData);
-router.get('/data/:val', getData);
-router.put('/data', putData);
-
-async function getData(req: Request) {
-  const data = await ns1.get('test');
-  return response.json(data);
-}
-
-async function putData(req: Request) {
-  const body = await req.json();
-  const result = await ns1.put('test', JSON.stringify(body));
-  return response.json({ body: result });
-}
-
-async function generateRandomNumber(req: Request) {
-  return response.json({ data: random() });
-}
-
-export { router };
+// export { router };
